aa39b729985fbcc53dcf0553cd17cc3e752a9b23
davide-amabile <davideamabile81@gmail.com>
2020-08-06 17:51:01 +0200

    controllo sesso

--- js/script.js
+++ js/script.js
@@ -10,12 +10,20 @@ if (gender === "maschio") {
   document.getElementById('name').classList.add("blue");
 }
 else if (gender === "femmina"){
-  document.getElementById('name').classList.add("pink");  
+  document.getElementById('name').classList.add("pink");
 }
+
 
 document.getElementById('name').innerHTML = (" " + nome);
 
 
+ if (gender === "maschio" || gender === "femmina"){
+   document.getElementById("name").innerHTML = ( " "+ nome );
+ }
+  else if ( gender != "maschio" || gender != "femmina"){
+  document.getElementById('name').innerHTML = ( " " + "immettere il sesso");
+
+}
 
 
 
