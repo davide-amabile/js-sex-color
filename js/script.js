13f846fe4724c7a6667f0215a41d74a030477f98
davide-amabile <davideamabile81@gmail.com>
2020-08-06 18:20:42 +0200

    fine controlli

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 // chiedere il nome e il sesso
-
 var nome = prompt("Come ti chiami?");
 var gender = prompt("Sei maschio o femmina?");
 
 // azzurro se maschio rosa se è femmina
-
-
 if (gender === "maschio") {
   document.getElementById('name').classList.add("blue");
 }
@@ -17,17 +14,11 @@ else {
   document.getElementById('name').classList.add("display");
 }
 
-
-document.getElementById('name').innerHTML = (" " + nome);
-
-
-//  if (gender === "maschio" || gender === "femmina"){
-//    document.getElementById("name").innerHTML = ( " "+ nome );
-//  }
-//   else if ( gender != "maschio" || gender != "femmina"){
-//   document.getElementById('name').innerHTML = ( " " + "immettere il sesso");
-// }
- if (!isNaN(nome) || !isNaN(gender)){
-   alert("Ricontrolla i dati inserimento");
-   document.getElementById('name').classList.add("display");
+// controllo numeri
+if (!isNaN(nome) || !isNaN(gender)){
+  alert("Solo lettere!");
+  document.getElementById('name').classList.add("display");
 }
+
+// stampa a video
+document.getElementById('name').innerHTML = (" " + nome);
