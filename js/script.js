22fb71c76ab62a6345604729c92e65d5c8ce25a6
davide-amabile <davideamabile81@gmail.com>
2020-08-06 18:14:45 +0200

    controllo numeri

--- js/script.js
+++ js/script.js
@@ -12,47 +12,22 @@ if (gender === "maschio") {
 else if (gender === "femmina"){
   document.getElementById('name').classList.add("pink");
 }
+else {
+  alert("Ricontrolla i dati inserimento");
+  document.getElementById('name').classList.add("display");
+}
 
 
 document.getElementById('name').innerHTML = (" " + nome);
 
 
- if (gender === "maschio" || gender === "femmina"){
-   document.getElementById("name").innerHTML = ( " "+ nome );
- }
-  else if ( gender != "maschio" || gender != "femmina"){
-  document.getElementById('name').innerHTML = ( " " + "immettere il sesso");
-
+//  if (gender === "maschio" || gender === "femmina"){
+//    document.getElementById("name").innerHTML = ( " "+ nome );
+//  }
+//   else if ( gender != "maschio" || gender != "femmina"){
+//   document.getElementById('name').innerHTML = ( " " + "immettere il sesso");
+// }
+ if (!isNaN(nome) || !isNaN(gender)){
+   alert("Ricontrolla i dati inserimento");
+   document.getElementById('name').classList.add("display");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if (gender === "maschio" || gender === "femmina"){
-//   document.getElementById("name").innerHTML = ( " "+ nome );
-// }
-//  else if ( gender != "maschio" || gender != "femmina"){
-//  document.getElementById('name').innerHTML = ("immettere il sesso");
-//
-// }
-//
-//
-// if (isNaN(nome)){
-//   document.getElementById('name').innerHTML = (" " + nome );
-// }
-// else if (!isNaN(nome)){
-//   document.getElementById('name').innerHTML = (" " + "immettere il nome");
-// }
